02e6130c48d4244b1fc68c28f1108e894bf26cee
chenpoyang <young.cpy@gmail.com>
2012-04-23 21:55:52 +0800

    modified:   log/process_note 	modified:   src/progress.c 	modified:   src/progress.h 	modified:   src/tags

--- src/progress.c
+++ src/progress.c
@@ -141,8 +141,9 @@ void get_spd(const int tm_stm, const int got_bytes, char *ret_spd)
 {
 	/* B,K,M,G/s */
 	char *ch = "BKMG";
+	char str[4];
 	char spd;
-	int index, res, head, tail, unit;
+	int index, res, head, tail, unit, len;
 
 	assert(ret_spd != NULL);
 
@@ -174,7 +175,7 @@ void get_spd(const int tm_stm, const int got_bytes, char *ret_spd)
 		unit = 1024 * 1024 * 1024;
 	}
 	
-	/* 计算速度值 */
+	/* 计算速度值, 占八个字符,第一个字符为空格!, eg: | 29.1M/s| */
 	while (res > unit)
 	{
 		res -= unit;
@@ -182,18 +183,165 @@ void get_spd(const int tm_stm, const int got_bytes, char *ret_spd)
 	tail = res;
 	head = got_bytes - tail;
 	index = 0;
-	while (index < 4 && head > 0)
+	ret_spd[index++] = ' ';
+	if ('M' == spd || 'G' == spd)
 	{
+		/* head, eg: part of "29.1M/s" */
+		m_utoa(head, str);
+		len = strlen(str);
+		strncpy(ret_spd + index, str, len);
+		index += len;
+		ret_spd[index++] = '.';
+
+		/* tail, eg: part of "29.1M/s" */
+		m_utoa(tail * 10 / unit, str);	/* 小数点后的整数 */
+		len = strlen(str);
+		strncpy(ret_spd + index, str, len);
+		index += len;
+		while (index < 5)
+		{
+			ret_spd[index++] = ' ';
+		}
+		ret_spd[indesx] = '\0';
 	}
+	else
+	{
+		m_utoa(head, str);
+		len = strlen(str);
+		strncpy(ret_spd + index, str, len);
+		index += len;
+
+		if (index <= 3)
+		{
+			ret_spd[index++] = '.';
+			m_utoa(tail * 10 / unit, str);
+			ret_spd[index++] = *str;
+		}
+
+		while (index < 5)
+		{
+			ret_spd[index++] = ' ';
+		}
+		ret_spd[index] = '\0';
+	}
+
+	ret_spd[index++] = spd;
+	ret_spd[index++] = '/';
+	ret_spd[index++] = 's';
+	ret_spd[index] = '\0';
+}
+
+void get_rec_bytes(const int done, char *ret)
+{
+	char str[9], *ptr = NULL;	/* | ___,___,___| */
+	int index, len;
+
+	index = 0;
+	ret[index++] = ' ';
+	m_utoa(done, str);
+	len = strlen(str);
+	ptr = str;
+	while (len > 3)
+	{
+		strncpy(ret + index, ptr, 3);
+		index += 3;
+		ret[index++] = ',';
+		len -= 3;
+		ptr += 3;
+	}
+	strncpy(ret + index, ptr, len);
+	index += len;
+	ret[index] = '\0';
+}
+
+/* get the bar: [------->        ] 
+ * width = 4 + 1 + left + 1 + 12 + 8 + 14, default width: 85, left = 45!
+ * width: 总的宽度
+ * perc: 百分整, eg: the interger of 80%, 85%, 75% etc
+ */
+void get_ptr_bar(int perc, const int width, char *ret)
+{
+	int index, left, head, tail;
+
+	index = 0;
+	ret[index++] = '[';
+
+	left = width - 4 - 1 - 1 - 12 - 8 - 14;
+	head = perc * width / 100;
+	tail = left - tail;
+	while (--head > 0)
+	{
+		ret[index++] = '-';
+	}
+	ret[index++] = '>';
+
+	while (--tail >= 0)
+	{
+		ret[index++] = ' ';
+	}
+
+	ret[index++] = ']';
+	ret[index] = '\0';
+}
+/*
+ *|<------14---->|
+ *|  ------------|
+ *|  in  --.--   |
+ *|  in  01s     |
+ *|  in  12m 05s |
+ *|  eta 01d 16h |
+ *|<------14---->|
+ *显示状态, 如剩余时间, 完成任务的总花时等, 占14个字符, 包括前面两空格字符!
+ */
+void get_status(const int tm_stm, const int done, const int totals, char *ret)
+{
+	int index, h, m, s;
+	char tmp_str[10];
+
+	index = 0;
+	ret[index++] = ' ';
+	ret[index++] = ' ';
+	if (done >= 0 && done != totals)
+	{
+		ret[index++] = 'i';
+		ret[index++] = 'n';
+		ret[index++] = ' ';
+		ret[index++] = ' ';
+
+		/* 时分秒 */
+		s = tm_stm;
+		h = s / 60 / 60;
+		s -= h * 60 * 60;
+		m = s / 60;
+		s -= m * 60;
+	}
+	else if (done >= 0 && done < totals)
+	{
+		ret[index++] = 'e';
+		ret[index++] = 't';
+		ret[index++] = 'a';
+		ret[index++] = ' ';
+	}
+
+	while (index < 14)
+	{
+		ret[index++] = ' ';
+	}
+	ret[index] = '\0';
 }
 
 void create_image(Progress *pro, const int width)
 {
 	char *p = NULL;
+	char spd[8], rec_bytes[12], per[4];
 
 	assert(pro != NULL && width > 0);
 
 	p = pro->buf;
+	get_spd(pro->end_sec - pro->beg_sec, pro->done, spd);
+	get_percent(pro->done, pro->total, per);
+	get_rec_bytes(pro->done, rec_bytes);
+	get_ptr_bar(pro->done * 100 / pro->total, width, p + 4);
 }
 
 void display_image(Progress *pro)
